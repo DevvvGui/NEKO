6e22c84058b360530f06ffdc3efb337e8c81fa60
DevvvGui <guiilherme1803@gmail.com>
2021-08-10 15:37:14 -0300

    changes

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import websockets
 import asyncio
 
+runnings = 0
+
 async def start():
+    global runnings
+    
     async with websockets.connect('wss://web.whatsapp.com/ws', origin = 'https://web.whatsapp.com', ping_interval = None) as socket:
         while True:
-            await socket.send('emgejsks,["admin","init",[0,3,2390],["Long browser description","ShortBrowserDesc"],"enbekeksuenb",true]')
+            runnings += 1
+            
+            await socket.send(f'running-{runnings},["admin","init",[0,3,2390],["Long browser description","ShortBrowserDesc"],"enbekeksuenb",true]')
             message = await socket.recv()
             print(message)
         
-            await asyncio.sleep(5)
+            await asyncio.sleep(10)
         
 asyncio.run(start())
