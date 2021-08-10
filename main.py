e900f65dc5cd025f1ada8f15db8671a6bcd4c4ef
DevvvGui <guiilherme1803@gmail.com>
2021-08-10 15:04:34 -0300

    main

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import asyncio
 async def start():
     async with websockets.connect('wss://web.whatsapp.com/ws', origin = 'https://web.whatsapp.com') as socket:
             while True:
+                asyncio.sleep(5)
+
                 message = socket.recv()
                 print(message)
-                
-                asyncio.sleep(5)
                 
 asyncio.run(start())
