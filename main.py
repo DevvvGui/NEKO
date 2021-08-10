7502ca2f7f4ddfcc6b6212241c52e7e6bfe02ee0
DevvvGui <guiilherme1803@gmail.com>
2021-08-10 15:40:58 -0300

    changes

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import websockets
 import asyncio
-
-runnings = 0
+from datetime import datetime
 
 async def start():
-    global runnings
-    
     async with websockets.connect('wss://web.whatsapp.com/ws', origin = 'https://web.whatsapp.com', ping_interval = None) as socket:
         while True:
-            runnings += 1
-            
-            await socket.send(f'running-{runnings},["admin","init",[0,3,2390],["Long browser description","ShortBrowserDesc"],"enbekeksuenb",true]')
-            message = await socket.recv()
-            print(message)
-        
+            await socket.send(f'{datetime.now().timestamp()},["admin","init",[0,3,2390],["Long browser description","ShortBrowserDesc"],"enbekeksuenb",true]')
+            print(await socket.recv())
             await asyncio.sleep(10)
         
 asyncio.run(start())
