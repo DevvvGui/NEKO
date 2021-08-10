25137547fb619182f23f6c025cfa3ed4660f1d45
DevvvGui <guiilherme1803@gmail.com>
2021-08-10 15:33:36 -0300

    changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import websockets
 import asyncio
 
 async def start():
-    async with websockets.connect('wss://web.whatsapp.com/ws', origin = 'https://web.whatsapp.com') as socket:
+    async with websockets.connect('wss://web.whatsapp.com/ws', origin = 'https://web.whatsapp.com', ping_interval = None) as socket:
         while True:
             await socket.send('emgejsks,["admin","init",[0,3,2390],["Long browser description","ShortBrowserDesc"],"enbekeksuenb",true]')
             message = await socket.recv()
